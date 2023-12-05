7071a048c0ade7a0bce1ce36095651adba09bd6e
SepdeGraaff <sep.de.graaff@maxserv.com>
2023-12-05 17:13:48 +0100

    [Portfolio website]Fixed minor background animation issue

--- public/scripts/projects-animation.js
+++ public/scripts/projects-animation.js
@@ -4,12 +4,22 @@ projectHolders.forEach((holder) => {
     const project = holder.querySelector('.project');
 
     holder.addEventListener('mouseenter', () => {
-        project.classList.remove('w-0', 'opacity-0');
-        project.classList.add('w-full', 'opacity-95');
+        project.classList.remove('w-0');
+        project.classList.add('w-full');
+
+        project.classList.remove('opacity-0');
+        project.classList.add('opacity-100');
+
+        project.classList.add('bg-[#075985]/80')
     });
 
     holder.addEventListener('mouseleave', () => {
-        project.classList.remove('w-full', 'opacity-95');
-        project.classList.add('w-0', 'opacity-0');
+        project.classList.remove('w-full');
+        project.classList.add('w-0');
+
+        project.classList.remove('opacity-100');
+        project.classList.add('opacity-0');
+
+        project.classList.remove('bg-[#075985]/80')
     });
 });
