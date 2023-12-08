eb1625012a34b8da324b66a1bf4fb2161c003198
SepdeGraaff <sep.de.graaff@maxserv.com>
2023-12-08 13:33:39 +0100

    [Portfolio website]Menu done for now

--- public/scripts/menu-animation.js
+++ public/scripts/menu-animation.js
@@ -12,4 +12,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const menu = document.querySelectorAll('.menu');
 
     menu.forEach((el) => MenuDetector.observe(el));
+
+    const menuOpen = document.getElementById("menuOpen");
+    const menuLinks = document.querySelectorAll('.menuLink');
+    const menuId = document.getElementById("menu");
+
+    function toggleMenu() {
+        menuOpen.classList.toggle("flex");
+        menuOpen.classList.toggle("hidden");
+
+        menuId.classList.toggle("hidden");
+        menuId.classList.toggle("flex");
+    }
+
+    menuOpen.addEventListener("click", toggleMenu);
+
+    menuLinks.forEach((el) => {
+        el.addEventListener("click", toggleMenu);
+    });
 });
